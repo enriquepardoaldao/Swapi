f48e3ff7e45196f6bcbaf97121f496e75c6021fe
Enrique Pardo Aldao <enrique.pardo@udc.es>
2020-06-27 13:58:48 +0200

    first commit

---
+++ src/main/java/com/prueba/Trileuco/TrileucoApplication.java
@@ -0,0 +1,13 @@
+package com.prueba.Trileuco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrileucoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrileucoApplication.class, args);
+	}
+
+}
